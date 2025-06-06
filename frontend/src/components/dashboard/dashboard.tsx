17a3acc64417d886c42093b0e99aa182d9ea8cfc
pampers23 <dexterpimss23@gmail.com>
2025-06-06 08:07:39 +0800

    feat: add/fix chart area design

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -7,8 +7,10 @@ const Dashboard = () => {
     <div>
       <div className="flex h-full w-full flex-col items-center justify-start gap-4 p-4">
         <HeaderSection />
+        <div className="h-full w-full max-w-7xl flex-1 overflow-y-auto mt-5">
+          <ChartAreaInteractive />
+        </div>
       </div>
-      <ChartAreaInteractive />
     </div>
   )
 }
